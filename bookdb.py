32c57f12db65bfb62e8c50e6a1638a7899178d4f
sota_masuda <masuda.kyushu@gmail.com>
2019-08-23 14:35:32 +0900

    QR コード生成機能作成完了, flake8のwarning対応(一部), sqlite3処理のリファクタリング

--- bookdb.py
+++ bookdb.py
@@ -1,11 +1,10 @@
 import sqlite3
-from contextlib import closing
 import pandas as pd
 
-#=========================================================
+# =========================================================
 # 蔵書データベース管理クラス
 # CRUDを担う
-#=========================================================
+# =========================================================
 # https://gray-code.com/php/create-table-by-using-sqlite3/
 # https://www.dbonline.jp/sqlite/table/index9.html#section1
 # https://qiita.com/mas9612/items/a881e9f14d20ee1c0703
@@ -13,9 +12,11 @@ import pandas as pd
 # https://blog.amedama.jp/entry/sqlite3-in-memory-issue
 # https://stackoverflow.com/questions/5801170/python-sqlite-create-table-if-not-exists-problem
 # https://riptutorial.com/ja/pandas/example/5586/%E3%82%BF%E3%83%97%E3%83%AB%E3%81%AE%E3%83%AA%E3%82%B9%E3%83%88%E3%81%8B%E3%82%89dataframe%E3%82%92%E4%BD%9C%E6%88%90%E3%81%99%E3%82%8B
-#---------------------------------------------------------
+# https://pythondatascience.plavox.info/pandas/%E8%A1%8C%E3%83%BB%E5%88%97%E3%81%AE%E6%8A%BD%E5%87%BA
+# https://pythondatascience.plavox.info/pandas/%E8%A1%8C%E3%83%BB%E5%88%97%E3%81%AE%E9%95%B7%E3%81%95%E3%82%92%E7%A2%BA%E8%AA%8D
+# ---------------------------------------------------------
 
-#-----------------------------------
+# -----------------------------------
 # DB Architect
 #  - column0 : unique id
 #  - column1 : isbn
@@ -27,15 +28,15 @@ import pandas as pd
 # 1 : checked out(貸出中)
 # 2 : disposed(廃棄済)
 # ***********
-#-----------------------------------
+# -----------------------------------
+
 
 class BookDB:
-    
-    def __init__(self,dbname="database.db"):
+    def __init__(self, dbname="database.db"):
         self.database_name = dbname
         self.conn = sqlite3.connect(dbname)
         self.c = self.conn.cursor()
-    
+
     def __del__(self):
         self.c.close()
         self.conn.close()
@@ -60,7 +61,7 @@ class BookDB:
         bookinfo = (isbn, title, status)
         self.c.execute(query, bookinfo)
         self.conn.commit()
-    
+
     # Insert records
     #  > books is list of touple like this >> [(isbn,title,status),(),()...]
     def insert_book_records(self, books=[]):
@@ -68,15 +69,25 @@ class BookDB:
         self.c.executemany(query, books)
         self.conn.commit()
 
-    # Test Read DB
+    # Select all data with tablename
     def select_table_alldata_as_df(self, tablename="books"):
         query = 'select * from ' + tablename
-        records = []
-        for rec in self.c.execute(query):
-            records.append(rec)
-        
+        records = self.c.execute(query).fetchall()
+        if records is None:
+            return None
         df = pd.DataFrame(records)
         return df
 
-
-
+    # Select last N data with tablename
+    def select_table_lastNdata_as_df(self, tablename="books", datanum=0):
+        # check args
+        if datanum == 0:
+            return None
+        # check data num
+        alldata = self.select_table_alldata_as_df(tablename)
+        all_row_size = len(alldata.index)
+        if (all_row_size == 0) or (all_row_size < datanum):
+            return None
+        # get last N data
+        target_data = alldata[(all_row_size - 1 - datanum):(all_row_size - 1)]
+        return target_data
--- registerbooks.py
+++ registerbooks.py
@@ -1,18 +1,28 @@
 # 新規購入書籍をDBに登録するアプリケーション
+# https://note.nkmk.me/python-pillow-qrcode/
+# https://note.nkmk.me/python-pandas-dataframe-for-iteration/
+# https://qiita.com/Kenta-Han/items/e64035e9c3e4ef08e394#%E6%96%87%E5%AD%97%E5%88%97%E3%81%AE%E5%88%87%E3%82%8A%E5%87%BA%E3%81%97
 
 import requests
-import pandas as pd
-import json
 from codereader import CodeReader
 from bookdb import BookDB
+import qrcode
+import pathlib
+
 
 class RegisterBooks:
     BOOK_DB_NAME = "book_database.db"
     GBOOKS_API_URL = 'https://www.googleapis.com/books/v1/volumes?q=isbn:'
+    QR_CODE_IMG_DIR = './qrcode_files'
+
     def __init__(self):
         self.reader = CodeReader()
         self.db = BookDB(RegisterBooks.BOOK_DB_NAME)
         self.db.create_table()
+        outdir = pathlib.Path(RegisterBooks.QR_CODE_IMG_DIR)
+        if not outdir.exists():
+            outdir.mkdir()
+
     # def __del__(self):
     #     pass
 
@@ -20,7 +30,7 @@ class RegisterBooks:
         req_url = RegisterBooks.GBOOKS_API_URL + str(isbn)
         response = requests.get(req_url)
         return response
-    
+
     def get_booktitle_from_gbsapi(self, json_data):
         try:
             title = json_data['items'][0]['volumeInfo']['title']
@@ -45,16 +55,16 @@ class RegisterBooks:
 
     # main process loop until input ESC
     def start(self):
-        #===========================================================
+        # ===========================================================
         # scanning books and getting books' info phase
-        #===========================================================
+        # ===========================================================
         registerd_count = 0
         self.print_start_msg()
         while True:
-            #----------------------------------------------------
+            # ----------------------------------------------------
             # Get ISBN code
-            #----------------------------------------------------
-            ret,code = self.reader.read()
+            # ----------------------------------------------------
+            ret, code = self.reader.read()
             # カメラからISBNコードをとれない場合は手入力
             if ret == -1:
                 print("input isbn code with keyboard ...")
@@ -64,34 +74,44 @@ class RegisterBooks:
             elif ret == -2:
                 print("save and quit ... ")
                 break
-            #----------------------------------------------------
+            # ----------------------------------------------------
             # Get Book's info with ISBN code
-            #----------------------------------------------------
+            # ----------------------------------------------------
             response = self.search_book_with_isbn(code)
             json_data = response.json()
             book_title = self.get_booktitle_from_gbsapi(json_data)
             # APIから本のタイトルが取得できない場合は手入力
-            if book_title == None:
+            if book_title is None:
                 print('sorry cant find that book ... isbn = ' + str(code))
                 print('input book title , please.')
                 book_title = input()
             # 登録する情報を表示する
             print("isbn: " + str(code) + ", title: " + str(book_title))
 
-            #----------------------------------------------------
+            # ----------------------------------------------------
             # Register for DB
-            #----------------------------------------------------
-            self.db.insert_book_record(isbn=str(code), title=book_title, status=0)
+            # ----------------------------------------------------
+            self.db.insert_book_record(
+                isbn=str(code), title=book_title, status=0)
             registerd_count += 1
 
-        #===========================================================
+        # ===========================================================
         # generate book's unique QR code image
-        #===========================================================
-        # test implementation ***
-        print("registerd count is : " + str(registerd_count))
-        recs = self.db.select_table_alldata_as_df()
-        print(recs)
-
+        # ===========================================================
+        registered_data = self.db.select_table_lastNdata_as_df(datanum=registerd_count)
+        if registered_data is None:
+            return
+        for row in registered_data.itertuples():
+            # QR code string
+            qrcode_str = str(row[1]) + "_" + str(row[2])
+            # QR code image file name
+            title_str = str(row[3]).strip()[:10]
+            qrcode_filename = str(row[1]) + "_" + title_str + ".png"
+            # output QR code png file
+            qrcode_img = qrcode.make(qrcode_str)
+            qrcode_img.save(RegisterBooks.QR_CODE_IMG_DIR + '/' + qrcode_filename)
+            # debug code
+            print(row)
 
 
 if __name__ == "__main__":
